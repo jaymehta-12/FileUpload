8c878d6ad022476949c9fc66887695fb31e9a2fc
Jay Mehta <jaymehta-12>
2020-02-27 19:15:18 +0530

    Updated

---
+++ FileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using FileUpload.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using File = FileUpload.Model.File;
+
+namespace FileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult UploadFile( IFormFile File)
+        {
+            string store = "C:\\Users\\jmehta\\Documents\\Text";
+
+            var file = Request.Form.Files[0];
+
+            var memory = new MemoryStream();
+
+            file.CopyTo(memory);
+
+            System.IO.File.WriteAllBytes(store + "\\" + file.FileName, memory.ToArray());
+
+
+
+
+            return Ok();
+           
+        }
+    }
+}
